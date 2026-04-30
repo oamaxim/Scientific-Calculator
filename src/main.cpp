3feadcbe0d187ef8322369102d03294f3e5f7669
Toba <270317238+bl4-k@users.noreply.github.com>
2026-04-30 15:58:26 +0100

    feat: updated the test in main to test the capabilities of the parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include "lexer/Lexer.h"
+#include "parser/Parser.h"
 
 int main()
 {
     std::string input;
-    std::getline(std::cin, input);
 
-    Lexer lexer(input);
-    auto tokens = lexer.tokenise();
-
-    for (auto token : tokens)
+    while (true)
     {
-        std::cout << static_cast<int>(token.type)
-                  << " "
-                  << token.value
-                  << std::endl;
+        std::cout << "> ";
+        std::getline(std::cin, input);
+
+        if (input == "exit")
+            break;
+
+        try
+        {
+            Lexer lexer(input);
+            std::vector<Token> tokens = lexer.tokenise();
+
+            Parser parser(tokens);
+            double result = parser.parse();
+
+            std::cout << "= " << result << std::endl;
+        }
+        catch (const std::exception &e)
+        {
+            std::cout << "Error: " << e.what() << std::endl;
+        }
     }
 
     return 0;
